c2c75b6b018e149cf770cc5ab665cf2b2a30b604
GGoIn <goearthend@outlook.com>
2025-06-15 10:38:31 +0800

    20250615-1038-1a

---
+++ Binary_search/P2678.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a[50001];
+//!===================================!
+int main(){
+    int l,n,m;
+    scanf("%d%d%d",&l,&n,&m);
+    for(int i=1;i<=n;i++){
+        scanf("%d",&a[i]);
+    }
+    int l=0,r=l,mid,ans;
+    
+    return 0;
+}
