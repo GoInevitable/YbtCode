8b412d6928ed85110ffeed9e991284b83f9173e4
GGoIn <goearthend@outlook.com>
2025-07-06 16:37:34 +0800

    20250706-1a-LT

---
+++ Binary_search/P1102-luogu.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a[200001];
+int main(){
+    int n,c;
+    scanf("%d",&n,&c);
+    for(int i=1;i<=n;i++){
+        scanf("%d",&a[i]);
+    }
+    return 0;
+}
