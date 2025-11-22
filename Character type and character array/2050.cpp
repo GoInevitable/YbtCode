d3ed7d1a2cb92d00631b1ebf2f70d8ae800394bc
GGoIn <goearthend@outlook.com>
2025-11-22 09:46:35 +0800

    20251112-1a

---
+++ Character type and character array/2050.cpp
@@ -0,0 +1,14 @@
+/** 
+ * @author  Jane Smith
+ * @file    2050.cpp
+ * @date    2025-11-星期六
+ * @version 1.0
+ * @problem problem_name
+ * @license MIT
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    return 0;
+}
